2b9a4ab6a98518f9b9771bea8c10b6da438a85dd
jamescasia <jamescasia99@gmail.com>
2023-10-26 13:51:06 +0800

    Update constants period tokens

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,10 +1,10 @@
 // ignore_for_file: non_constant_identifier_names
 
 class Constants {
-  static const String TWENTYFOUR_HOURS = '24H';
-  static const String SEVEN_DAYS = '7D';
-  static const String THIRTY_DAYS = '30D';
-  static const String NINETY_DAYS = '90D';
+  static const String TWENTYFOUR_HOURS = '1d';
+  static const String SEVEN_DAYS = '7d';
+  static const String THIRTY_DAYS = '3d';
+  static const String NINETY_DAYS = '90d';
   static const String ONE_YEAR = '1Y';
   static const List<String> months = [
     '',
