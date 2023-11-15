7727eb9e9a712aa562920d44ded7bceafeabd67e
jamescasia <jamescasia99@gmail.com>
2023-11-15 15:01:22 +0800

    Show tooltip initially

--- lib/charts/single_chart.dart
+++ lib/charts/single_chart.dart
@@ -112,17 +112,17 @@ class _SingleChartState extends State<SingleChart> {
         });
 
     currencyFormat.maximumFractionDigits = 2;
-
+    WidgetsFlutterBinding.ensureInitialized();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // WidgetsBinding.instance.addPostFrameCallback((_) {
-    // Timer(const Duration(milliseconds: 1000), () {
-    _tooltipBehavior.showByIndex(0, widget.data.length - 1);
-    // });
-    // });
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Timer(const Duration(milliseconds: 1000), () {
+        _tooltipBehavior.showByIndex(0, widget.data.length - 1);
+      });
+    });
     // calculate min max
     double max_ = double.negativeInfinity;
     double min_ = double.infinity;
